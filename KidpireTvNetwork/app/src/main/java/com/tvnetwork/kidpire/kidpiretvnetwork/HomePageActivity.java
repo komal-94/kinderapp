07762b21ffc128edf03be25c265f84d2c3eb86aa
komall-webonise <komal493549@gmail.com>
2017-08-18 11:02:11 +0530

    completed version 1

--- KidpireTvNetwork/app/src/main/java/com/tvnetwork/kidpire/kidpiretvnetwork/HomePageActivity.java
+++ KidpireTvNetwork/app/src/main/java/com/tvnetwork/kidpire/kidpiretvnetwork/HomePageActivity.java
@@ -1,13 +1,40 @@
 package com.tvnetwork.kidpire.kidpiretvnetwork;
 
+import android.graphics.Color;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class HomePageActivity extends AppCompatActivity {
-
+    String TAG = "com.ebookfrenzy.videoplayer";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        final VideoView videoView =
+                (VideoView) findViewById(R.id.videoView);
+        String path = "android.resource://" + getPackageName() + "/"
+                + R.raw.beautiful_video;
+        videoView.setVideoURI(Uri.parse(path));
+        MediaController mediaController = new
+                MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setBackgroundColor(Color.WHITE);
+        videoView.setOnPreparedListener(new
+                                                MediaPlayer.OnPreparedListener()  {
+                                                    @Override
+                                                    public void onPrepared(MediaPlayer mp) {
+                                                        //Log.i(TAG, "Duration = " + videoView.getDuration());
+                                                        videoView.setZOrderOnTop(false);
+                                                        videoView.setBackgroundColor(Color.TRANSPARENT);
+                                                    }
+                                                });
+        videoView.setZOrderOnTop(true);
+        videoView.start();
     }
 }
